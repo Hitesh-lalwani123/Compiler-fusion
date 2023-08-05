e73b45834b278b1c8b7acd76fcebc86d7d914770
Hitesh <hlalwani28022001@gmail.com>
2023-08-05 14:45:22 +0530

    added language support,added fully functional compiler and standard input

--- client/src/App.js
+++ client/src/App.js
@@ -1,101 +1,107 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-import axios from 'axios'
-import qs from 'qs'
-// import EditorComponent from "./EditorComponent";
+import axios from "axios";
 import Editor from "@monaco-editor/react";
-
+import Spinner from "./components/Spinner";
+import languages from "./constraints/Languages";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  const [data1, setData] = useState(null);
 
   const [code, setCode] = useState("");
-  const [output,setOutput] = useState('');
-  var data = qs.stringify({
-    code: code,
-    language: "cpp",
-    input: "12",
-  });
-  var config = {
-    method: "post",
-    url: "https://api.codex.jaagrav.in",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    data: data,
-  };
-  const [loading,setLoading] = useState('');
+  const [output, setOutput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState();
+  const [language,setLanguage] = useState('cpp');
+  const [input,setInput] = useState('');
 
-  const handleClick = async () => {
-    console.log(code);
-    setLoading('loading...');
-    let a = await axios(config)
+  const handleClick = () => {
+
+    setLoading("loading...");
+    axios.post('http://localhost:3001/submissions', {
+      code:code,
+      input:input,
+      lang:language
+    })
       .then(function (response) {
-        console.log(response.data.output);
-        setData(response.data.output);
+        console.log(response);
         setOutput(response.data.output);
-        setLoading('');
+        setLoading(false);
       })
       .catch(function (error) {
         console.log(error);
+        setOutput("Error in execution");
+        setLoading(false);
       });
-
   };
   const handleChange = (e) => {
-    setCode(e.target.value);
+    
+    setCode(e);
+    socket.emit("send-message", e);
   };
 
-  const [users, setUsers] = useState();
+  const handleLanguageChange = (option)=>{
+    setLanguage(option.target.value);
+    console.log(option.target.value)
+  }
+
+  const handleInput = (e)=>{
+    setInput(e.target.value);
+    console.log(e.target.value);
+  }
+
 
   useEffect(() => {
     socket.on("receive-message", (data) => {
-      setCode(code + data);
+      setCode(data);
     });
   }, [socket]);
+
   socket.on("user-change", (data) => {
     setUsers(data);
   });
 
-  socket.on("new-connection", (users) => {
-    setCode(users);
+  socket.on("new-connection", (data) => {
+    setCode(data);
+
   });
 
-  const style = {
-    background: "beige",
-    width: "80%",
-    display: "flex",
-  };
   return (
-    <div className="container">
-      <div style={style}>Your Personalised Code collaborator</div>
-
+    <div className="bg-blue-300 h-full">
       
+      <select name="myselect" id="selection" onChange={(e)=>{handleLanguageChange(e)}}>
+        <option value="cpp">c++</option>
+        <option value="python3">python</option>
+        <option value="javascript">Javascript</option>
+        <option value="java">java</option>
+      </select>
+      <div className="main grid grid-cols-4">
+        <div className="editor col-span-3 m-2">
           <Editor
             className="text-xl"
-            height="85vh"
-            width={"80%"}
-            defaultLanguage="c++"
+            height='90vh'
+            defaultLanguage="cpp"
+            language={language}
+            defaultValue=""
             value={code}
             theme="vs-dark"
-            options={{ fontSize: 20 }}
-            onChange={(e) => {
-              console.log("dsfa");
-              console.log(e);
-
-              setCode(e);
-              socket.emit("send-message", e);
-            }}
+            options={{ fontSize: 12 }}
+            onChange={(e) => {handleChange(e)}}
           />
-      
-
+        </div>
+        <div className="sidebar mt-2 mr-2 mb-2 rounded-md grid grid-rows-6">
+          <div className="output bg-white rounded-md m-2 row-span-3 min-w-0 grid grid-rows-6">
+            {output}
+            <div className="row-span-1 row-start-6">
+              {loading && <Spinner />}
+            </div>
+          </div>
+          <textarea className="input bg-white rounded-md m-1 row-span-2 p-2 font-thin text-black text-sm" value={input} onChange={(e)=>{handleInput(e)}}></textarea>
+          <button className="row-span-1 text-white bg-blue-600 rounded-md md:w-40 h-6 mt-4 ml-2 mr-2 text-sm font-semibold hover:bg-blue-700 hover:text-gray-neutral-300" onClick={handleClick}>Submit</button>
+        </div>
+      </div>
       <div>Active Users Connected: {users}</div>
-      <button onClick={handleClick}>Click me</button>
-
-      <div>Output</div>
-      {loading}
-      {output}
     </div>
   );
 }
